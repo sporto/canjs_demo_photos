83b08f0af6c6528b4a643e6ca895201373bb9283
Sebastian <sebasporto@gmail.com>
2013-04-09 09:01:02 +1000

    libraries

--- js/app.js
+++ js/app.js
@@ -14,64 +14,21 @@
 		init: function (ele, options) {
 			var self = this;
 
-			// state observable
-			this.state = new can.Observe({
-				selected: new Library(),
-				isEditing: function () {
-					return this.attr('selected.id') != null;
-				}
-			});
-
 			// create an empty list to be populated later
 			this.libraries = new Library.List([]);
 
 			// compile the template
-			var template = can.view("#template", {libraries: this.libraries, state: this.state});
+			var template = can.view("#template", {libraries: this.libraries});
 			
 			// append the view to the DOM
 			can.$(ele).append(template);
-
-			// store a ref to the input field
-			this.$input = $('.input_name', ele);
 
 			// load the libraries from the server
 			var pro = Library.findAll({}, function(libraries) {
 				self.libraries.push.apply(self.libraries, libraries);
 			});
 
-			// listen for when the selected model is changed
-			// reset the inputs
-			this.state.bind('selected', function (oldVal, newVal) {
-				self.$input.val(newVal.attr('name'));
-			});
-
 		},
-
-		/*********************
-		 * Methods
-		 *********************/
-
-		resetSelected: function () {
-			this.state.attr('selected', new Library());
-		},
-
-		/*********************
-		* UI Bindings
-		*********************/
-
-		'.btn_edit click': function (ele, ev) {
-			// get the clicked model from the DOM
-			var library = ele.data('library');
-
-			// flag the model as dirty
-			library.backup();
-
-			// set this as the currently selected model
-			this.state.attr('selected', library);
-
-			return false;
-		},
-
 
 		'.btn_save click': function (ele, ev) {
 			var self = this;
@@ -86,7 +43,6 @@
 					self.libraries.push(library);
 				}
 
-				self.resetSelected();
 			});
 
 			return false;
@@ -97,21 +53,8 @@
 			// get the model from the clicked element and destroy it
 			var library = can.data(ele, 'library');
 			library.destroy();
-			this.resetSelected();
+			
 			return false;
-		},
-
-
-		'.btn_cancel_edit click': function (ele, ev) {
-			this.state.attr('selected').restore();
-			this.resetSelected();
-			return false;
-		},
-
-
-		'.input_name keyup': function (ele, ev) {
-			var val = ele.val();
-			this.state.attr('selected.name', val);
 		}
 
 	});
