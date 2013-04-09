86ee45e61ba53b1982ca6e9b7256592bab869321
Sebastian <sebasporto@gmail.com>
2013-04-10 07:16:35 +1000

    Clean up

--- js/app.js
+++ js/app.js
@@ -18,6 +18,11 @@
 
 			// create an empty list to be populated later
 			this.items = new Item.List([]);
+			
+			// load the libraries from the server
+			var pro = Item.findAll({}, function(items) {
+				self.items.replace(items);
+			});
 
 			// compile the template
 			var template = can.view("#template", {items: this.items});
@@ -25,30 +30,26 @@
 			// append the view to the DOM
 			can.$(ele).append(template);
 
-			// load the libraries from the server
-			var pro = Item.findAll({}, function(items) {
-				self.items.replace(items);
-			});
-
 		},
 
 		'.btn_add click': function (ele, ev) {
 			var self = this;
 			
-			var n = _.random(0, randoms.length -1)
-			var img = 'img/' + randoms.splice(n, 1)[0] + '.png';
-			
-			var model = new Item({img: img});
+			var model = new Item({img: this.getNewItem()});
 			model.save(function (item) {
 				self.items.push(item);
 			});
 		},
 
-
 		'.btn_remove click': function (ele, ev) {
 			// get the model from the clicked element and destroy it
 			var item = can.data(ele, 'item');
 			item.destroy();
+		},
+		
+		getNewItem: function () {
+			var n = _.random(0, randoms.length -1);
+			return 'img/' + randoms.splice(n, 1)[0] + '.png';
 		}
 
 	});
